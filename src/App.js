fc3a9f75940023201f8d1b4677b186eac5be28a5
sobPare <143205056+sobPare@users.noreply.github.com>
2024-01-02 17:13:00 -0800

    okeyed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Weather from "./Weather";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello mf</h1>
+        <h1>Weahter App</h1>
+        <Search />
+
         <Weather city="paris" />
       </header>
     </div>
